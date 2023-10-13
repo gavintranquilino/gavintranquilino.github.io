a5cf23227abc070ce810be01fb4ae809e0bca02e
Gavin T <gavintranquilino@gmail.com>
2023-10-13 14:47:57 -0400

    Hockey Bot

--- img_size.py
+++ img_size.py
@@ -1,15 +1,6 @@
 from PIL import Image
 import os
 
-def get_dimensions(image):
-    while True:
-        try:
-            length = int(input("Enter the horizontal (X-axis) distance (in pixels): "))
-            width = int(input("Enter the vertical (Y-axis) distance (in pixels): "))
-            return length, width
-        except ValueError:
-            print("Invalid input. Please enter valid integers for the horizontal and vertical distances.")
-
 def suggest_new_size(image):
     current_width, current_height = image.width, image.height
     suggested_width = current_width
@@ -37,16 +28,19 @@ def resize_images(input_dir, size_range):
                 target_size = (size_range, size_range)
             else:  # If the image is not square, suggest a new size
                 original_width, original_height = image.width, image.height
-                print(f"Image '{filename}' is not square.")
-                print(f"Original Dimensions: {original_width} (X dist) x {original_height} (Y dist)")
                 suggested_width, suggested_height = suggest_new_size(image)
-                print(f"Suggested Size: {suggested_width} (X dist) x {suggested_height} (Y dist)")
-                change_size = input("Do you want to change the image size (Y/N)? ").strip().lower()
+                
+                # Skip if the suggested size matches the original size
+                if (suggested_width, suggested_height) == (original_width, original_height):
+                    print(f"Skipped: {filename} - Size matches the suggested image size.")
+                    continue
+                
+                # If the user wants to change the size, apply the suggested size
+                change_size = input(f"Image '{filename}' is not square. Current Size: {original_width} x {original_height}. Do you want to change the image size to {suggested_width} x {suggested_height} (Y/N)? ").strip().lower()
                 if change_size == 'n':
                     target_size = (image.width, image.height)  # Keep the original size
                 else:
-                    length, width = get_dimensions(image)
-                    target_size = (length, width)
+                    target_size = (suggested_width, suggested_height)
 
             resized_image = image.resize(target_size, Image.LANCZOS)  # Use LANCZOS resampling for high quality
             resized_image.save(input_path)
